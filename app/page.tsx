5da936f7ba83454cdac234540a33a51721e586db
Anietie Brownson <anni.brownson@gmail.com>
2024-11-14 12:59:24 +0100

    feat: Add about image

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Hero from "@/public/hero.png";
-import Avatar1 from "@/public/avatar1.png"
-import Avatar2 from "@/public/avatar2.png"
-import Avatar3 from "@/public/avatar3.png"
+import About from "@/public/about-image.png";
+import Avatar1 from "@/public/avatar1.png";
+import Avatar2 from "@/public/avatar2.png";
+import Avatar3 from "@/public/avatar3.png";
 import ArrowRight from "@/public/icons/circle-arrow-right-01.png";
-import Oval from "@/public/icons/oval.png"
+import Oval from "@/public/icons/oval.png";
 import Cards from "@/components/HeroCards";
 
 export default function Home() {
@@ -37,10 +38,14 @@ export default function Home() {
             </div>
           </div>
           <div className="mt-5 md:mt-8 lg:mt-10 flex justify-center relative -z-30">
-          <Image src="/icons/oval.png" alt="" width={119.7} height={119.7} />
-          {/* </div> */}
+            <Image src="/icons/oval.png" alt="" width={119.7} height={119.7} />
+            {/* </div> */}
 
-            <Image src={Hero} alt="Hero image of Clientyne dashboard" className="hidden md:block md:top-14 absolute" />
+            <Image
+              src={Hero}
+              alt="Hero image of Clientyne dashboard"
+              className="hidden md:block md:top-14 absolute"
+            />
           </div>
         </div>
       </section>
@@ -49,68 +54,90 @@ export default function Home() {
         <div className="container flex flex-col items-center justify-center px-4 md:px-8 lg:px-20">
           <div className="relative flex justify-evenly items-center w-full max-w-[451px] py-4 text-[#F5F5F5] px-2 rounded-full bg-[#0E0E0D]">
             <div className="flex w-full max-w-[88px] -mt-10">
-              <Image src={Avatar1} alt="" className="w-5 h-5 lg:w-10 lg:h-10 object-contain absolute left-5" />
-              <Image src={Avatar2} alt="" className="w-6 h-6 lg:w-12 lg:h-12 object-contain absolute left-10" />
-              <Image src={Avatar3} alt="" className="w-6 h-6 lg:w-12 lg:h-12 object-contain absolute left-[3.8rem]" />
+              <Image
+                src={Avatar1}
+                alt=""
+                className="w-5 h-5 lg:w-10 lg:h-10 object-contain absolute left-5"
+              />
+              <Image
+                src={Avatar2}
+                alt=""
+                className="w-6 h-6 lg:w-12 lg:h-12 object-contain absolute left-10"
+              />
+              <Image
+                src={Avatar3}
+                alt=""
+                className="w-6 h-6 lg:w-12 lg:h-12 object-contain absolute left-[3.8rem]"
+              />
             </div>
 
-        <div className="text-sm text-center font-medium font-onest">
-          Ready to onboard{" "}
-          <span className="font-semibold text-[#FFF]">10,000+</span> businesses
-        </div>
-        </div>
+            <div className="text-sm text-center font-medium font-onest">
+              Ready to onboard{" "}
+              <span className="font-semibold text-[#FFF]">10,000+</span>{" "}
+              businesses
+            </div>
+          </div>
 
-        <div className="max-w-[766px] mx-auto text-center">
-        <div className="mt-20 flex flex-col justify-center items-center">
-          <h1 className="text-[#3D3D3D] font-redHat font-black text-2xl lg:text-4xl lg:leading-snug max-w-[500px]">
-            Smart solutions for your{" "}
-            <span className="text-[#626262]">
-              business growth
-            </span>
-          </h1>
-          <p className="mt-5 md:mt-6 text-sm font-onest text-[#626262] leading-[20.4px] max-w-[590px]">
-            {" "}
-            we’re dedicated to providing powerful tools tailored to small and
-            medium businesses. Our team works hard to give you easy, effective
-            tools designed to help your business grow.
-          </p>
-        </div>
-        </div>
-        <div className="mt-10 w-full">
-          <Cards />
-        </div>
+          <div className="max-w-[766px] mx-auto text-center">
+            <div className="mt-20 flex flex-col justify-center items-center">
+              <h1 className="text-[#3D3D3D] font-redHat font-black text-2xl lg:text-4xl lg:leading-snug max-w-[500px]">
+                Smart solutions for your{" "}
+                <span className="text-[#626262]">business growth</span>
+              </h1>
+              <p className="mt-5 md:mt-6 text-sm font-onest text-[#626262] leading-[20.4px] max-w-[590px]">
+                {" "}
+                we’re dedicated to providing powerful tools tailored to small
+                and medium businesses. Our team works hard to give you easy,
+                effective tools designed to help your business grow.
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 w-full">
+            <Cards />
+          </div>
         </div>
       </section>
 
+      {/* about section */}
       <section className="mt-20 lg:mt-20 flex flex-col lg:flex-row justify-between items-center">
-        <div className="container px-4 md:px-8 lg:px-20">
-          <h1 className="font-redHat lg:text-4xl leading-relaxed mb-5">
-            <span className="text-[#626262]">Clientyne:</span> Built for your business growth
-          </h1>
-          <p className="text-[#626262] font-onest lg:text-base mt-3">
-            Clientyne is a customer management platform designed to empower
-            small and medium businesses. We offer intuitive tools to streamline
-            customer interactions, gain insights, and manage essential business
-            tasks like invoicing and analytics.
-          </p>
-          <p className="text-[#3D3D3D] font-onest lg:text-base mt-3">
-            Our mission is to simplify business management for small and medium
-            businesses, helping them grow through efficient, user-friendly
-            tools.
-          </p>
-          <p className="text-[#626262] font-onest lg:text-base mt-3">
-            Our vision is to become the go-to platform for businesses worldwide,
-            transforming customer management into a seamless and empowering
-            experience.
-          </p>
+        <div className="container flex flex-col lg:flex-row justify-between items-center px-4 md:px-8 lg:px-20">
+          <div className="flex flex-col order-last lg:order-first max-w-[533px]">
+            <h1 className="font-redHat lg:text-4xl leading-relaxed mb-5">
+              <span className="text-[#626262]">Clientyne:</span> Built for your
+              business growth
+            </h1>
+            <p className="text-[#626262] font-onest lg:text-base mt-3">
+              Clientyne is a customer management platform designed to empower
+              small and medium businesses. We offer intuitive tools to
+              streamline customer interactions, gain insights, and manage
+              essential business tasks like invoicing and analytics.
+            </p>
+            <p className="text-[#3D3D3D] font-onest lg:text-base mt-3">
+              Our mission is to simplify business management for small and
+              medium businesses, helping them grow through efficient,
+              user-friendly tools.
+            </p>
+            <p className="text-[#626262] font-onest lg:text-base mt-3">
+              Our vision is to become the go-to platform for businesses
+              worldwide, transforming customer management into a seamless and
+              empowering experience.
+            </p>
+          </div>
+          <Image src={About} alt="" className="lg:w-[528px] lg:h-[440px]" />
         </div>
       </section>
 
       <section className="mt-10 md:mt-20 lg:mt-40">
         <div className="container px-4 md:px-8 lg:px-28">
           <div className="py-10 px-8 lg:py-20 text-center bg-[#333333] text-[#FFF] rounded-xl max-w-[1036px]">
-            <h1 className="font-redHat text-xl font-extrabold lg:text-4xl mb-5">Join us early and grow your business</h1>
-            <p className="font-onest font-medium text-base lg:text-[18px] max-w-[699px] mx-auto">Clientyne is launching soon, and we're welcoming 10,000+ businesses in our first stage. Join us early and let’s grow together!</p>
+            <h1 className="font-redHat text-xl font-extrabold lg:text-4xl mb-5">
+              Join us early and grow your business
+            </h1>
+            <p className="font-onest font-medium text-base lg:text-[18px] max-w-[699px] mx-auto">
+              Clientyne is launching soon, and we're welcoming 10,000+
+              businesses in our first stage. Join us early and let’s grow
+              together!
+            </p>
             <div className="mt-6 lg:mt-11 flex justify-center">
               <Button className="flex items-center justify-center bg-[#008080] py-6 px-4 rounded-full">
                 Book a demo{" "}
